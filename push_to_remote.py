a305827fcad4adfdb65ff867d173bba018667e2a
wzsvip <161473552+wzsvip@users.noreply.github.com>
2024-10-29 22:33:04 +0800

    Add files via upload

--- push_to_remote.py
+++ push_to_remote.py
@@ -2,6 +2,10 @@ import os
 import subprocess
 
 def main():
+    # 配置 Git 用户身份信息
+    subprocess.run(['git', 'config', '--local', 'user.email', 'wuzhij@mail.com'])
+    subprocess.run(['git', 'config', '--local', 'user.name', 'wzsvip'])
+
     # 添加更改并提交
     subprocess.run(['git', 'add', '.'])
     commit_message = "Unzipped files from main directory"
@@ -10,8 +14,6 @@ def main():
     # 推送更改到远程仓库
     remote_url = "https://github.com/wzsvip/git.git"
     subprocess.run(['git', 'remote', 'set-url', 'origin', remote_url])
-    
-    # 使用 PAT 进行身份验证
     subprocess.run(['git', 'push', 'origin', 'main'], env={'GIT_ASKPASS': '', 'GIT_TERMINAL_PROMPT': '0', 'GIT_SSH_COMMAND': 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'})
     print("Pushed changes to remote repository")
     
